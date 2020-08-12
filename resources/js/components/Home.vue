d124d00d76d0fd45ac7435f7dd31dd72dd3a606a
Nur Waqid Muhsinin <waqid.id@gmail.com>
2020-08-12 11:28:31 +0700

    Initial commit

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>   
+    <nav class="flex items-center justify-between flex-wrap bg-gray-300 p-6">
+        <a href="https://laravel.com/docs">Docs</a>
+        <a href="https://laracasts.com">Laracasts</a>
+        <a href="https://laravel-news.com">News</a>
+        <a href="https://blog.laravel.com">Blog</a>
+        <a href="https://nova.laravel.com">Nova</a>
+        <a href="https://forge.laravel.com">Forge</a>
+        <a href="https://vapor.laravel.com">Vapor</a>
+        <a href="https://github.com/laravel/laravel">GitHub</a>
+    </nav>
+   </div>
+</template>
+
---
+++ resources/views/layouts/vue.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+        <link rel="stylesheet" href="{{ asset('./css/app.css') }}">
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="text-center px-4 m-12 text-4xl ">
+                    Laravel + Vue + Tailwind + PurgeCSS
+                </div>
+                <div id="app"></div>
+                <script src="{{ asset('js/app.js') }}"></script>
+            </div>
+        </div>
+    </body>
+</html>
